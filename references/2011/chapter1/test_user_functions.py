67e02779ac9884f013c7b53b40c1d775968725ba
Luciano Ramalho <luciano@ramalho.org>
2018-11-18 21:10:04 -0200

    removed nose dependency from old example

--- references/2011/chapter1/test_user_functions.py
+++ references/2011/chapter1/test_user_functions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from nose.tools import eq_, raises, assert_raises
+from pytest import raises
 
 from kamin1 import *
 
@@ -17,40 +17,37 @@ def test_install_function():
     func_def = parse_source('(define dobro (n) (* n 2))')
     eva.install_function(*func_def[1:])
 
-@raises(ReservedIdentifier)
 def test_install_function_reserved_name():
-    func_def = parse_source('(define if (n) (* n 2))')
-    eva.install_function(*func_def[1:])
+    with raises(ReservedIdentifier):
+        func_def = parse_source('(define if (n) (* n 2))')
+        eva.install_function(*func_def[1:])
 
 def test_bind_function_args():
     body = parse_source('(* n 2)')
     double = UserFunction('double', ['n'], body)
-    eq_(double.bind_args([3]), {'n':3})
+    assert double.bind_args([3]) == {'n':3}
 
 def test_install_and_use_function():
     func_def = parse_source('(define dobro (n) (* n 2))')
     eva.install_function(*func_def[1:])
     expr = parse_source('(dobro 4)')
-    eq_(eva.evaluate({}, expr), 8)
+    assert eva.evaluate({}, expr) == 8
 
 def test_install_and_use_function_arity_2():
     func_def = parse_source('(define mod (m n) (- m (* n (/ m n))))')
     eva.install_function(*func_def[1:])
     expr = parse_source('(mod 11 4)')
-    eq_(eva.evaluate({}, expr), 3)
+    assert eva.evaluate({}, expr) == 3
 
-@raises(MissingArguments)
 def test_install_and_use_function_arity_2_missing_arg():
-    func_def = parse_source('(define mod (m n) (- m (* n (/ m n))))')
-    eva.install_function(*func_def[1:])
-    expr = parse_source('(mod 11)')
-    eq_(eva.evaluate({}, expr), 3)
+    with raises(MissingArguments):
+        func_def = parse_source('(define mod (m n) (- m (* n (/ m n))))')
+        eva.install_function(*func_def[1:])
+        expr = parse_source('(mod 11)')
+        assert eva.evaluate({}, expr) == 3
 
 def test_install_and_use_recursive_function():
     func_def = parse_source('(define ! (n) (if (< n 2) 1 (* n (! (- n 1)))))')
     eva.install_function(*func_def[1:])
     expr = parse_source('(! 5)')
-    eq_(eva.evaluate({}, expr), 120)
-
-
-
+    assert eva.evaluate({}, expr) == 120
